77bf048864d1172a0f7c84167ad1afef617f308f
ChandulaNethmal <38277605+ChandulaNethmal@users.noreply.github.com>
2019-08-21 13:38:58 +0530

    Update statemachine.py

--- statemachine.py
+++ statemachine.py
@@ -2,7 +2,7 @@ class StateMachine:
     def __init__(self):
         self.handlers = {}
         self.startState = None
-        self.endStates = []
+        self.endStates = ["COMPLETED_TASK"]
 
     def add_state(self, name, handler, end_state=0):
         name = name.upper()
@@ -13,16 +13,16 @@ class StateMachine:
     def set_start(self, name):
         self.startState = name.upper()
 
-    def run(self, cargo):
-        try:
-            handler = self.handlers[self.startState]
-        except:
-            raise InitializationError("must call .set_start() before .run()")
-        if not self.endStates:
-            raise  InitializationError("at least one state must be an end_state")
+    def run(self):
+       # try:
+        handler = self.handlers[self.startState]
+        #except:
+        #    raise InitializationError("must call .set_start() before .run()")
+        #if not self.endStates:
+        #    raise  InitializationError("at least one state must be an end_state")
     
         while True:
-            (newState, cargo) = handler(cargo)
+            (newState) = handler()
             if newState.upper() in self.endStates:
                 print("reached ", newState)
                 break 
